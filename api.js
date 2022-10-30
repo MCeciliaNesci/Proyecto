f93351038bb1b55c86d4b24f981d557abdc0ceec
Silvina79 <silvinaglew@gmail.com>
2022-10-30 19:46:38 -0300

    b

--- api.js
+++ api.js
@@ -1,1 +1,12 @@
+ document.getElementById('email').addEventListener('input', function () {
+                    campo = event.target;
+                    valido = document.getElementById('emailOK');
 
+                    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+                   
+                    if (emailRegex.test(campo.value)) {
+                        valido.innerText = "válido";
+                    } else {
+                        valido.innerText = "incorrecto";
+                    }
+                });
